b8c16682adade598f55b7ac047accb612cdce1d6
akpalinov <chelseazhiti@gmail.com>
2024-04-24 12:32:45 +0500

    Initial Commit

---
+++ HelloWorldBaha/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorldBaha
+//
+//  Created by Bakhtiyar Akpalinov on 24.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
